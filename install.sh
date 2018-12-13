98e15cba90f84d4ffc86bba247355fe3c9b94ae2
Tiago Matana <tiago.matana@gmail.com>
2018-12-13 12:23:30 -0200

    Mudança no script para construção do container

--- install.sh
+++ install.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+DARKGRAY='\033[1;30m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+printf "\n\n * * * NGINX * * * \n"
+printf "JENKINS HOST(default = ${YELLOW}localhost${NC}): "
+read host
+if [ -z $host ]; then
+    host='localhost'
+fi
+sed -i "s/localhost/$host/g" ./nginx/default.conf
+
 sudo docker-compose down
 sudo rm -rf jenkins/home/*
 sudo cp -R jenkins/backup/* jenkins/home/
