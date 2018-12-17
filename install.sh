ce1e57f21e4d41d044b696f085d15b1905d6f5d4
Tiago Matana <tiago.matana@gmail.com>
2018-12-17 17:47:01 -0200

    Metodo CopyOnWrite aplicado

--- install.sh
+++ install.sh
@@ -9,15 +9,19 @@ NC='\033[0m' # No Color
 
 printf "\n\n * * * NGINX * * * \n"
 printf "JENKINS HOST(default = ${YELLOW}localhost${NC}): "
-read host
-if [ -z $host ]; then
-    host='localhost'
+read jenkins
+if [ -z $jenkins ]; then
+    jenkins='localhost'
 fi
-sed -i "s/localhost/$host/g" ./nginx/default.conf
 
 sudo docker-compose down
-sudo rm -rf jenkins/home/*
-sudo cp -R jenkins/backup/* jenkins/home/
+
+sudo mkdir ./nginx/persistence/
+sudo cp -R ./nginx/config/ ./nginx/persistence/
+sed -i "s/localhost/$jenkins/g" ./nginx/persistence/sites-enabled/jenkins.conf
+
+sudo mkdir ./jenkins/persistence/
+sudo cp -R jenkins/backup/* jenkins/persistence/
 sudo docker-compose up -d
 sudo docker exec -it jenkins /bin/bash ./entrypoint.sh
 sudo docker restart jenkins
